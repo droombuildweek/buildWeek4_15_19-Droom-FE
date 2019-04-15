1bf65b5b34305339d26844f73d574686ebe03278
Mckay <mckaywrigley@gmail.com>
2019-04-15 13:04:12 -0400

    creates form for register and login components

--- client/src/components/auth/register.js
+++ client/src/components/auth/register.js
@@ -7,87 +7,28 @@ class Register extends Component {
       <>
         <Form>
           <FormGroup>
-            <Label for="exampleEmail">Email</Label>
-            <Input
-              type="email"
-              name="email"
-              id="exampleEmail"
-              placeholder="with a placeholder"
-            />
+            <Label for="email">Email</Label>
+            <Input type="email" name="email" id="email" placeholder="email" />
           </FormGroup>
           <FormGroup>
-            <Label for="examplePassword">Password</Label>
+            <Label for="password">Password</Label>
             <Input
               type="password"
               name="password"
-              id="examplePassword"
-              placeholder="password placeholder"
+              id="password"
+              placeholder="password"
             />
           </FormGroup>
           <FormGroup>
-            <Label for="exampleSelect">Select</Label>
-            <Input type="select" name="select" id="exampleSelect">
-              <option>1</option>
-              <option>2</option>
-              <option>3</option>
-              <option>4</option>
-              <option>5</option>
-            </Input>
-          </FormGroup>
-          <FormGroup>
-            <Label for="exampleSelectMulti">Select Multiple</Label>
+            <Label for="password2">Confirm Password</Label>
             <Input
-              type="select"
-              name="selectMulti"
-              id="exampleSelectMulti"
-              multiple
-            >
-              <option>1</option>
-              <option>2</option>
-              <option>3</option>
-              <option>4</option>
-              <option>5</option>
-            </Input>
+              type="password"
+              name="password2"
+              id="password 2"
+              placeholder="confirm password"
+            />
           </FormGroup>
-          <FormGroup>
-            <Label for="exampleText">Text Area</Label>
-            <Input type="textarea" name="text" id="exampleText" />
-          </FormGroup>
-          <FormGroup>
-            <Label for="exampleFile">File</Label>
-            <Input type="file" name="file" id="exampleFile" />
-            <FormText color="muted">
-              This is some placeholder block-level help text for the above
-              input. It's a bit lighter and easily wraps to a new line.
-            </FormText>
-          </FormGroup>
-          <FormGroup tag="fieldset">
-            <legend>Radio Buttons</legend>
-            <FormGroup check>
-              <Label check>
-                <Input type="radio" name="radio1" /> Option one is this and
-                that—be sure to include why it's great
-              </Label>
-            </FormGroup>
-            <FormGroup check>
-              <Label check>
-                <Input type="radio" name="radio1" /> Option two can be something
-                else and selecting it will deselect option one
-              </Label>
-            </FormGroup>
-            <FormGroup check disabled>
-              <Label check>
-                <Input type="radio" name="radio1" disabled /> Option three is
-                disabled
-              </Label>
-            </FormGroup>
-          </FormGroup>
-          <FormGroup check>
-            <Label check>
-              <Input type="checkbox" /> Check me out
-            </Label>
-          </FormGroup>
-          <Button>Submit</Button>
+          <Button>Sign Up</Button>
         </Form>
       </>
     );
