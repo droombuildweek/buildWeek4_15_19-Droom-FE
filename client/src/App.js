580dab5325115a8c7738c87a177009bbf7e74637
Mckay <mckaywrigley@gmail.com>
2019-04-15 12:51:30 -0400

    initializes redux store

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,42 @@
 import React, { Component } from "react";
+// React Router
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
+// Redux
+import { createStore, applyMiddleware, compose } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import rootReducer from "./reducers";
+const store = createStore(
+  rootReducer,
+  compose(
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
+
+// Components
 import Header from "./components/nav/Header";
-import Footer from "./components/nav/Footer";
+// import Footer from "./components/nav/Footer";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 
+// Styling
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <>
-        <Router>
-          <Header />
-          {/* <Footer /> */}
+        <Provider store={store}>
+          <Router>
+            <Header />
+            {/* <Footer /> */}
 
-          <Route exact path="/register" component={Register} />
-          <Route exact path="/login" component={Login} />
-        </Router>
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </Router>
+        </Provider>
       </>
     );
   }
