41ab863a60e1f9a3c6ab7131d81db8c564a9e149
Mckay <mckaywrigley@gmail.com>
2019-04-15 12:13:34 -0400

    initial react router setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,26 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+import Header from "./components/nav/Header";
+import Footer from "./components/nav/Footer";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
+
 import "./App.css";
 
 class App extends Component {
   render() {
-    return <div className="App" />;
+    return (
+      <>
+        <Router>
+          <Header />
+          <Footer />
+
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+        </Router>
+      </>
+    );
   }
 }
 
