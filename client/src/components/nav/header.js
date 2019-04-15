4d6b040f36c5fe690c29cb8ae16a88b4c4522f92
Mckay <mckaywrigley@gmail.com>
2019-04-15 12:29:27 -0400

    creates initial header

---
+++ client/src/components/nav/header.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { Nav, NavItem, NavLink } from "reactstrap";
+
+class Header extends Component {
+  render() {
+    return (
+      <div>
+        <Nav>
+          <NavItem>
+            <NavLink href="/">Home</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="/login">Login</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="/register">Sign Up</NavLink>
+          </NavItem>
+        </Nav>
+        <hr />
+      </div>
+    );
+  }
+}
+
+export default Header;
