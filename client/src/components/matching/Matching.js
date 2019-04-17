c7d513cca0f72474ab8278026c457260ef51784b
Mckay <mckaywrigley@gmail.com>
2019-04-17 16:13:37 -0700

    creates carousel

--- client/src/components/matching/Matching.js
+++ client/src/components/matching/Matching.js
@@ -1,10 +1,40 @@
 import React, { Component } from "react";
+import Slider from "react-slick";
+
+import "./Matching.scss";
 
 class Matching extends Component {
   render() {
+    var settings = {
+      arrows: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      prevArrow: "match",
+      nextArrow: ""
+    };
     return (
       <div>
-        <h2>Matching</h2>
+        <Slider {...settings}>
+          <div>
+            <h3>1</h3>
+          </div>
+          <div>
+            <h3>2</h3>
+          </div>
+          <div>
+            <h3>3</h3>
+          </div>
+          <div>
+            <h3>4</h3>
+          </div>
+          <div>
+            <h3>5</h3>
+          </div>
+          <div>
+            <h3>6</h3>
+          </div>
+        </Slider>
       </div>
     );
   }
