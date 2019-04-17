bfa3948ed56ed419b23d6d00aceff9b85689ef84
Mckay <mckaywrigley@gmail.com>
2019-04-17 15:29:25 -0700

    creates initial matching component

---
+++ client/src/components/matching/Matching.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Matching extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Matching</h2>
+      </div>
+    );
+  }
+}
+
+export default Matching;
