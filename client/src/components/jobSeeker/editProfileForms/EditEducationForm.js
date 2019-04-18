b13a482aa7bca85f035859c59bd47e2ea70d56aa
Mckay <mckaywrigley@gmail.com>
2019-04-18 11:19:57 -0700

    gets edit education working

--- client/src/components/jobSeeker/editProfileForms/EditEducationForm.js
+++ client/src/components/jobSeeker/editProfileForms/EditEducationForm.js
@@ -1,17 +1,18 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { editSeekerEducation, getSeekerEducation } from "../../../actions";
+import _ from "lodash";
 
 class EditEducationForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
       userId: this.props.auth.user.subject,
-      eduSchool: this.props.seeker.seekerProfile.eduSchool,
-      eduCredential: this.props.seeker.seekerProfile.eduCredential,
-      eduDescription: this.props.seeker.seekerProfile.eduDescription,
-      eduStart: this.props.seeker.seekerProfile.eduStart,
-      eduEnd: this.props.seeker.seekerProfile.eduEnd,
+      eduSchool: "",
+      eduCredential: "",
+      eduDescription: "",
+      eduStart: "",
+      eduEnd: "",
       educations: []
     };
   }
@@ -27,97 +28,88 @@ class EditEducationForm extends Component {
     });
   };
 
-  addToArray = e => {
-    e.preventDefault();
-    const education = {
-      eduSchool: this.state.eduSchool,
-      eduCredential: this.state.eduCredential,
-      eduDescription: this.state.eduDescription,
-      eduStart: this.state.eduStart,
-      eduEnd: this.state.eduEnd
-    };
-    this.setState({
+  handleSubmit = id => {
+    const educationData = {
       eduSchool: "",
       eduCredential: "",
       eduDescription: "",
       eduStart: "",
       eduEnd: ""
-    });
-    this.state.educations.push(education);
-  };
-
-  handleSubmit = e => {
-    e.preventDefault();
-    const educationData = {
-      userId: this.state.userId,
-      seekerEducation: this.state.educations
     };
-    this.props.editSeekerEducation(
-      educationData,
-      this.props.seeker.seekerProfile.education.id
-    );
+    this.props.editSeekerEducation(educationData, id);
   };
 
   render() {
+    if (_.isEmpty(this.props.seeker.seekerProfile.education)) {
+      return <p>loading</p>;
+    }
     return (
       <div>
-        <form>
-          <h2>Edit Education</h2>
-          <div>
-            <label>School</label>
-            <input
-              name="eduSchool"
-              type="text"
-              placeholder="school"
-              value={this.state.eduSchool}
-              onChange={this.inputChange}
-            />
-          </div>
-          <div>
-            <label>Credential</label>
-            <input
-              name="eduCredential"
-              type="text"
-              placeholder="credential"
-              value={this.state.eduCredential}
-              onChange={this.inputChange}
-            />
-          </div>
-          <div>
-            <label>Description</label>
-            <input
-              name="eduDescription"
-              type="text"
-              placeholder="description"
-              value={this.state.eduDescription}
-              onChange={this.inputChange}
-            />
-          </div>
-          <div>
-            <label>Start</label>
-            <input
-              name="eduStart"
-              type="text"
-              placeholder="start"
-              value={this.state.eduStart}
-              onChange={this.inputChange}
-            />
-          </div>
-          <div>
-            <label>End</label>
-            <input
-              name="eduEnd"
-              type="text"
-              placeholder="end"
-              value={this.state.eduEnd}
-              onChange={this.inputChange}
-            />
-          </div>
-          <button onClick={this.addToArray}>Add Education</button>
-          <button type="submit" onClick={this.handleSubmit}>
-            Submit
-          </button>
-        </form>
+        <h2>Edit Education</h2>
+        {this.props.seeker.seekerProfile.education.map(education => {
+          return (
+            <div key={education.id}>
+              <p>{education.eduSchool}</p>
+              <p>{education.eduCredential}</p>
+              <p>{education.eduDescription}</p>
+              <p>{education.eduStart}</p>
+              <p>{education.eduEnd}</p>
+              <div>
+                <label>School</label>
+                <input
+                  name="eduSchool"
+                  type="text"
+                  placeholder="edit school"
+                  value={this.state.eduSchool}
+                  onChange={this.inputChange}
+                />
+              </div>
+              <div>
+                <label>Credential</label>
+                <input
+                  name="eduCredential"
+                  type="text"
+                  placeholder="edit credential"
+                  value={this.state.eduCredential}
+                  onChange={this.inputChange}
+                />
+              </div>
+              <div>
+                <label>Description</label>
+                <input
+                  name="eduDescription"
+                  type="text"
+                  placeholder="edit description"
+                  value={this.state.eduDescription}
+                  onChange={this.inputChange}
+                />
+              </div>
+              <div>
+                <label>Start</label>
+                <input
+                  name="eduStart"
+                  type="text"
+                  placeholder="edit start"
+                  value={this.state.eduStart}
+                  onChange={this.inputChange}
+                />
+              </div>
+              <div>
+                <label>End</label>
+                <input
+                  name="eduEnd"
+                  type="text"
+                  placeholder="edit end"
+                  value={this.state.eduEnd}
+                  onChange={this.inputChange}
+                />
+              </div>
+              <button onClick={e => this.handleSubmit(education.id)}>
+                Submit
+              </button>
+            </div>
+          );
+        })}
       </div>
     );
   }
