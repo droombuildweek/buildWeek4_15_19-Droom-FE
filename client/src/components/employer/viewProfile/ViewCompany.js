0ed9bdc575a92520581a493d499e066c1ace709e
Mckay <mckaywrigley@gmail.com>
2019-04-18 01:00:15 -0700

    renders compnay info in view component

--- client/src/components/employer/viewProfile/ViewCompany.js
+++ client/src/components/employer/viewProfile/ViewCompany.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { deleteEmployerCompany, getEmployerProfile } from "../../../actions";
+import _ from "lodash";
 
 class ViewCompany extends Component {
   componentDidMount() {
@@ -14,9 +15,21 @@ class ViewCompany extends Component {
   };
 
   render() {
+    if (_.isEmpty(this.props.employer.employerProfile)) {
+      return <p>loading</p>;
+    }
     return (
       <div>
         <h2>View Company</h2>
+        <p>{this.props.employer.employerProfile.companies.companyName}</p>
+        <p>{this.props.employer.employerProfile.companies.compnayPicture}</p>
+        <p>
+          {this.props.employer.employerProfile.companies.companyDescription}
+        </p>
+        <p>{this.props.employer.employerProfile.companies.country}</p>
+        <p>{this.props.employer.employerProfile.companies.state}</p>
+        <p>{this.props.employer.employerProfile.companies.city}</p>
+        <p>{this.props.employer.employerProfile.companies.zipcode}</p>
         <button onClick={this.deleteCompany}>Delete Company</button>
       </div>
     );
